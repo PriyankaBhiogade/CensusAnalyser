cd8c5d040b50c27e2c3a5555a92453962234d6d4
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-09 13:53:06 +0530

    Refactor : to seperate exception for CSV builder

--- src/main/java/censusanalyser/ICSVBuilder.java
+++ src/main/java/censusanalyser/ICSVBuilder.java
@@ -4,5 +4,5 @@ import java.io.Reader;
 import java.util.Iterator;
 
 public interface ICSVBuilder<E> {
-    public <E> Iterator<E> getCsvFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException;
-    }
+    public Iterator<E> getCsvFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
+}
