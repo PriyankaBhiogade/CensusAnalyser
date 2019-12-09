2f5a03c2b0b78ec86e53f8cd2695c046cbac293a
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-09 16:03:02 +0530

    Refactor : code to support List in place of Iterator

--- src/main/java/censusanalyser/ICSVBuilder.java
+++ src/main/java/censusanalyser/ICSVBuilder.java
@@ -2,7 +2,10 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder<E> {
     public Iterator<E> getCsvFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
+    public List<E> getCsvFileList(Reader reader, Class csvClass) throws CSVBuilderException;
+
 }
