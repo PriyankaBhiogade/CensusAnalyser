163c17379feaa87b2c10b41a229fa2435df52f5e
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-10 13:48:14 +0530

    Add : Code for RunTimeException in loadIndiaStateCode

--- src/main/java/com/censusanalyser/CensusAnalyserException.java
+++ src/main/java/com/censusanalyser/CensusAnalyserException.java
@@ -3,7 +3,7 @@ package com.censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE,NO_CENSUS_DATA
+        CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE,NO_CENSUS_DATA,NO_SUCH_TYPE
     }
 
     ExceptionType type;
