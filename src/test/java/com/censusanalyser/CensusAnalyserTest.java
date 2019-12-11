0b0c6eb0c659eccd255e4bf4b7644b12105c8f5e
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-11 09:32:14 +0530

    Add:test for sorting by population

--- src/test/java/com/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/censusanalyser/CensusAnalyserTest.java
@@ -186,4 +186,17 @@ public class CensusAnalyserTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.NO_CENSUS_DATA, e.type);
         }
     }
+
+    @Test
+    public void givenIndiaCensusData_WhenSortedOnPopulation_ShouldReturnSortedResult() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            String sortedCensusData = censusAnalyser.getPopulationWiseSortedCensusData();
+            IndiaCensusCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
+            Assert.assertEquals("Uttar Pradesh", censusCSV[28].state);
+        } catch (CensusAnalyserException e) {
+        }
+    }
+
 }
