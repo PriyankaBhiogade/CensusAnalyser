fe2b1e2e6081ce1678a9fad6afb4161a6cd08f53
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-13 10:29:42 +0530

    Refactor :Remove IndiaCensusAdapter & USCensusAdapter test

--- src/test/java/com/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/censusanalyser/CensusAnalyserTest.java
@@ -11,33 +11,6 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-    private static final String EMPTY_FILE_PATH = "./src/test/resources/EmptyFile.csv";
-    private static final String DELIMITER_FILE_PATH = "./src/test/resources/DelimiterWrong.csv";
-    private static final String HEADER_FILE_PATH = "./src/test/resources/HeaderMissing.csv";
-    private static final String US_CENSUS_FILE_PATH = "/home/admin1/Desktop/CensusAnalyser/CensusAnalyser/src/test/resources/USCensusData.csv";
-
-    @Test
-    public void givenIndiaCensusData_WithIncorrectFileName_ShouldThrowExceptionFromBuilder() {
-        try {
-            CensusAnalyser censusAnalyser = new CensusAnalyser();
-            ExpectedException exceptionRule = ExpectedException.none();
-            exceptionRule.expect(CensusAnalyserException.class);
-            censusAnalyser.loadCensusData(CensusAnalyser.Country.INDIA, INDIA_STATE_CODE_CSV_FILE_PATH);
-        } catch (CensusAnalyserException e) {
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.ERROR_FROM_CSV_BUILDER, e.type);
-        }
-    }
-
-    @Test
-    public void givenIndiaCensusData_WhenFilePathIsEmpty_ShouldReturnException() {
-        try {
-            CensusAnalyser censusAnalyser = new CensusAnalyser();
-            censusAnalyser.loadCensusData(CensusAnalyser.Country.INDIA, EMPTY_FILE_PATH);
-        } catch (CensusAnalyserException e) {
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.ERROR_FROM_CSV_BUILDER, e.type);
-        }
-    }
 
     @Test
     public void givenIndiaCensusData_WhenSortedOnState_ShouldReturnSortedResult() {
@@ -85,12 +58,5 @@ public class CensusAnalyserTest {
             Assert.assertEquals("Bihar", censusCSV[0].state);
         } catch (CensusAnalyserException e) {
         }
-    }
-
-    @Test
-    public void givenUSCensusData_ShouldReturnCorrectData() throws CensusAnalyserException {
-        CensusAnalyser censusAnalyser = new CensusAnalyser();
-        int numOfRecords = censusAnalyser.loadCensusData(CensusAnalyser.Country.US, US_CENSUS_FILE_PATH);
-        Assert.assertEquals(51, numOfRecords);
     }
 }
